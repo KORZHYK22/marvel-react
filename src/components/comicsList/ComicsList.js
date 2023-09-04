27db29ed41596cce3166d7b879dfe8162ae55726
Tetiana <kuzmenkotv22@gmail.com>
2023-09-04 16:18:22 -0600

    first commit

---
+++ src/components/comicsList/ComicsList.js
@@ -0,0 +1,70 @@
+import "./comicsList.scss";
+import { Link } from "react-router-dom";
+import useMarvelService from "../../services/MarvelServices";
+import { useState, useEffect } from "react";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+
+const ComicsList = (props) => {
+	const [offset, setOffset] = useState(210);
+	const [newItemLoading, setNewItemLoading] = useState(false);
+	const [comicsList, setComicsList] = useState([]);
+	const [comicsEnded, setComicsEnded] = useState(false);
+
+	const { loading, error, getAllComics } = useMarvelService();
+
+	useEffect(() => {
+		onRequest(offset, true);
+	}, []);
+
+	const onRequest = (offset, initial) => {
+		initial ? setNewItemLoading(false) : setNewItemLoading(true);
+		getAllComics(offset).then(onComicsListLoaded);
+	};
+
+	const onComicsListLoaded = (newComicsList) => {
+		let ended = false;
+		if (newComicsList.length < 8) {
+			ended = true;
+		}
+
+		setComicsList([...comicsList, ...newComicsList]);
+		setNewItemLoading((newItemLoading) => false);
+		setOffset(offset + 8);
+		setComicsEnded(ended);
+	};
+
+	function renderItems(arr) {
+		const items = arr.map((item, i) => {
+			return (
+				<li className="comics__item" key={i}>
+					<Link to={`/comics/${item.id}`}>
+						<img src={item.thumbnail} alt="ultimate war" className="comics__item-img" />
+						<div className="comics__item-name">{item.title}</div>
+						<div className="comics__item-price">{item.price} $</div>
+					</Link>
+				</li>
+			);
+		});
+
+		return <ul className="comics__grid">{items}</ul>;
+	}
+
+	const items = renderItems(comicsList);
+
+	const errorMessage = error ? <ErrorMessage /> : null;
+	const spinner = loading && !newItemLoading ? <Spinner /> : null;
+
+	return (
+		<div className="comics__list">
+			{errorMessage}
+			{spinner}
+			{items}
+			<button disabled={newItemLoading} style={{ display: comicsEnded ? "none" : "block" }} className="button button__main button__long" onClick={() => onRequest(offset)}>
+				<div className="inner">load more</div>
+			</button>
+		</div>
+	);
+};
+
+export default ComicsList;
---
+++ src/services/MarvelServices.js
@@ -0,0 +1,61 @@
+import { useHttp } from "../hooks/http.hooks";
+
+const useMarvelService = () => {
+	const { loading, error, clearError, request } = useHttp();
+
+	const _apiBase = "https://gateway.marvel.com:443/v1/public/";
+	const _apiKey = "apikey=58e8360b38a88134dd5765053b2023c1";
+	const _baseOffset = 210;
+
+	const getAllCharacters = async (offset = _baseOffset) => {
+		const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
+		return res.data.results.map(_transformCharacter);
+	};
+
+	const getCharacterByName = async (name) => {
+		const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+		return res.data.results.map(_transformCharacter);
+	};
+
+	const getAllComics = async (offset = _baseOffset) => {
+		const res = await request(`${_apiBase}comics?limit=8&offset=210&${_apiKey}`);
+		return res.data.results.map(_transformComics);
+	};
+
+	const getCharacter = async (id) => {
+		const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+		return _transformCharacter(res.data.results[0]);
+	};
+
+	const getComic = async (id) => {
+		const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+		return _transformComics(res.data.results[0]);
+	};
+
+	const _transformComics = (comics) => {
+		return {
+			id: comics.id,
+			title: comics.title,
+			description: comics.description || "There is no description",
+			thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+			pageCount: comics.pageCount ? `${comics.pageCount} p.` : "No information about the number of pages",
+			language: comics.textObjects[0]?.language || "en-us",
+			price: comics.prices[0].price ? `${comics.prices[0].price}` : "Not avalible",
+		};
+	};
+
+	const _transformCharacter = (char) => {
+		return {
+			id: char.id,
+			name: char.name,
+			description: char.description ? `${char.description.slice(0, 210)}...` : "There is no description for this character",
+			thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+			homepage: char.urls[0].url,
+			wiki: char.urls[1].url,
+			comics: char.comics.items,
+		};
+	};
+	return { loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic, getCharacterByName };
+};
+
+export default useMarvelService;
